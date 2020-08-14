46445b3a2745b621c35ab58db198addf8c4219e6
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-08-14 18:58:29 -0400

    registering and logging in forms

--- frontend/src/components/account_setup/LoginForm.tsx
+++ frontend/src/components/account_setup/LoginForm.tsx
@@ -1,9 +1,95 @@
 import React from "react";
 import { setTitleType } from "../App";
+import {
+  Theme,
+  createStyles,
+  makeStyles,
+  Container,
+  Typography,
+  Grid,
+  TextField,
+  Link,
+  Button,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    extraInfo: {
+      color: theme.palette.info.main,
+    },
+  })
+);
 
 export default ({ setTitle }: { setTitle: setTitleType }) => {
+  const classes = useStyles();
   React.useEffect(() => {
     setTitle("Login");
   }, [setTitle]);
-  return <div></div>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            autoFocus
+            color="secondary"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            color="secondary"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2" className={classes.extraInfo}>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2" className={classes.extraInfo}>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 };
--- frontend/src/components/theme.tsx
+++ frontend/src/components/theme.tsx
@@ -1,18 +1,25 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
+import {
+  amber,
+  deepPurple,
+  purple,
+} from "@material-ui/core/colors";
 
 export default createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: purple[900],
+      main: deepPurple[900],
     },
     secondary: {
-      main: purple[600],
+      main: purple[500],
     },
     background: {
       paper: "#2c2c3c",
       default: "#1c1c1e",
+    },
+    info: {
+      main: amber[900],
     },
   },
 });
