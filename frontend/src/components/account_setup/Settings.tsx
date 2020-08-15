4bc0bfa008a2e44ccf0e7bf381fb894304f7f050
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-08-14 20:00:11 -0400

    settings

--- frontend/src/components/account_setup/Settings.tsx
+++ frontend/src/components/account_setup/Settings.tsx
@@ -1,9 +1,85 @@
 import React from "react";
 import { setTitleType } from "../App";
+import {
+  Theme,
+  createStyles,
+  makeStyles,
+  Container,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    extraInfo: {
+      color: theme.palette.info.main,
+    },
+  })
+);
 
 export default ({ setTitle }: { setTitle: setTitleType }) => {
-    React.useEffect(() => {
-      setTitle("Settings");
-    }, [setTitle]);
-  return <div></div>;
+  React.useEffect(() => {
+    setTitle("Settings");
+  }, [setTitle]);
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Settings
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Display name"
+                autoFocus
+                color="secondary"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Email Address"
+                color="secondary"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Confirm
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 };
