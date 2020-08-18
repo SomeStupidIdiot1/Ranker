42fae9b72fa609caf94803c5e7ad168e001791dc
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-08-18 14:58:16 -0400

    table sql setup

--- backend/src/db/setup.sql
+++ backend/src/db/setup.sql
@@ -1,8 +1,20 @@
 CREATE TABLE IF NOT EXISTS accounts (
-	user_id serial PRIMARY KEY,
+	id serial PRIMARY KEY,
 	display_name VARCHAR ( 30 ) NOT NULL,
+	display_number SERIAL,
 	password_hash VARCHAR ( 100 ) NOT NULL,
 	email VARCHAR ( 100 ) UNIQUE NOT NULL,
 	created_on TIMESTAMP NOT NULL,
-  last_login TIMESTAMP 
+  last_login TIMESTAMP,
+	owner_of,
+	saved_as,
+);
+CREATE TABLE IF NOT EXISTS lists_of_items (
+	id serial PRIMARY KEY,
+	items text,
+	title VARCHAR(100) NOT NULL,
+	info VARCHAR(300),
+	creator NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+  last_updated TIMESTAMP 
 );
