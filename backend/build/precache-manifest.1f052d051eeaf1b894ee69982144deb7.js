4b421dce8180bdbac7b98efcedb736cda8cb98d2
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-09-05 14:48:37 -0400

    build

---
+++ backend/build/precache-manifest.1f052d051eeaf1b894ee69982144deb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77fe5258cacf4c35a05dddf586d9080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb89ba81a8f864b7fea0",
+    "url": "/static/css/2.27970ba2.chunk.css"
+  },
+  {
+    "revision": "cb89ba81a8f864b7fea0",
+    "url": "/static/js/2.b60d823b.chunk.js"
+  },
+  {
+    "revision": "964408f6508e96e219ed9aec3552bff7",
+    "url": "/static/js/2.b60d823b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5da8475d88fbfc6d1b4",
+    "url": "/static/js/main.571035f5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
